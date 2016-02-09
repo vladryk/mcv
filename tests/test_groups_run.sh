4ae421701e6b30c98368b27bdf57270bcfeaa9a2
Artem Yasakov <ayasakov@mirantis.com>
2016-02-09 17:14:38 +0300

    Fixed whitespace in bash script
    Change-Id: Ic31bc93231aff34fa833d87e39b9875f20959b80

--- tests/test_groups_run.sh
+++ tests/test_groups_run.sh
@@ -9,8 +9,8 @@ sudo sed -i "/\[basic\]/aos_password=${arr[4]}" /etc/mcv/mcv.conf
 sudo sed -i "/\[basic\]/aauth_endpoint_ip=${arr[5]}" /etc/mcv/mcv.conf
 sudo sed -i "/\[basic\]/anailgun_host=${arr[6]}" /etc/mcv/mcv.conf
 sudo sed -i "/\[basic\]/acluster_id=${arr[7]}" /etc/mcv/mcv.conf
-sudo sed -i "s/version = 6.1/version=${arr[8]}/" /etc/mcv/mcv.conf
-sudo sed -i "s/private_endpoint_ip = 192.168.0.2/private_endpoint_ip = ${arr[9]}/" /etc/mcv/mcv.conf
+sudo sed -i "s/version=6.1/version=${arr[8]}/" /etc/mcv/mcv.conf
+sudo sed -i "s/private_endpoint_ip=192.168.0.2/private_endpoint_ip=${arr[9]}/" /etc/mcv/mcv.conf
 sudo mcvconsoler --run custom resources >> /tmp/cli_output.log
 sudo echo 'resources: ' $? >> /tmp/test_result.log
 sudo mcvconsoler --run custom default >> /tmp/cli_output.log
