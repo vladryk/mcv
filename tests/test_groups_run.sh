0d9f7423e8afd5d44ff8777c9b408f983e89bad1
IlyaMenkov <imenkov@mirantis.com>
2016-02-24 17:11:44 +0300

    Add ability to run 5 test groups
    On CI will running 5 test groups now:
    - scale
    - workload
    - smoke
    - quick
    - functional
    
    Change-Id: Ibc19550db1c68a6bc87a3ecb9e6cf14ec768cf7e

--- tests/test_groups_run.sh
+++ tests/test_groups_run.sh
@@ -11,11 +11,14 @@ sudo sed -i "/\[basic\]/anailgun_host=${arr[6]}" /etc/mcv/mcv.conf
 sudo sed -i "/\[basic\]/acluster_id=${arr[7]}" /etc/mcv/mcv.conf
 sudo sed -i "s/version=6.1/version=${arr[8]}/" /etc/mcv/mcv.conf
 sudo sed -i "s/private_endpoint_ip=192.168.0.2/private_endpoint_ip=${arr[9]}/" /etc/mcv/mcv.conf
-sudo mcvconsoler --run custom resources >> /tmp/cli_output.log
-sudo echo 'resources: ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom default >> /tmp/cli_output.log
-sudo echo 'default: ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom shaker >> /tmp/cli_output.log
-sudo echo 'shaker: ' $? >> /tmp/test_result.log
+sudo sed -i "s/concurrency=5/concurrency=1/" /etc/mcv/mcv.conf
+sudo mcvconsoler --run custom quick >> /tmp/cli_output.log
+sudo echo 'quick ' $? >> /tmp/test_result.log
+sudo mcvconsoler --run custom workload >> /tmp/cli_output.log
+sudo echo 'workload ' $? >> /tmp/test_result.log
+sudo mcvconsoler --run custom functional >> /tmp/cli_output.log
+sudo echo 'functional ' $? >> /tmp/test_result.log
 sudo mcvconsoler --run custom smoke >> /tmp/cli_output.log
-sudo echo 'smoke: ' $? >> /tmp/test_result.log
+sudo echo 'smoke ' $? >> /tmp/test_result.log
+sudo mcvconsoler --run custom scale >> /tmp/cli_output.log
+sudo echo 'scale ' $? >> /tmp/test_result.log
