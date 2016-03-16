51184182eb6b570157b2ed1785f395e97f0caaf7
Oleksandr Kyrylchuk <okyrylchuk@mirantis.com>
2016-03-16 19:12:04 +0200

    Added module for error codes
    Reasons:
        - Magic constants are not the best example of coding experience
    Changes:
        - Added errors.py module
    Notes:
        - Replacement for error codes in sources has not been done yet
    
    Change-Id: Ifcea0e1161df98f8df7bc2ab01b4f5a458442b9f

---
+++ common/errors.py
@@ -0,0 +1,88 @@
+#    Copyright 2015 Mirantis, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class BaseError(object):
+
+    @staticmethod
+    def range():
+        raise NotImplementedError()
+
+class CAError(BaseError):
+    UNKNOWN_EXCEPTION = 11
+    RUNNER_LOAD_ERROR = 12
+    UNKNOWN_OUTER_ERROR = 13
+    WRONG_CREDENTIALS = 14
+
+    @staticmethod
+    def range():
+        return (1, 19)
+
+class SpeedError(BaseError):
+    LOW_AVG_SPEED = 22
+    FAILED_TEST_LIMIT_EXCESS = 29
+
+    @staticmethod
+    def range():
+        return (20, 29)
+
+class ResoureError(BaseError):
+    FAILED_TEST_LIMIT_EXCESS = 39
+
+    @staticmethod
+    def range():
+        return (30, 39)
+
+class ShakerError(BaseError):
+    TIMEOUT_EXCESS = 41
+    FAILED_TEST_LIMIT_EXCESS = 49
+
+    @staticmethod
+    def range():
+        return (40, 49)
+
+class RallyError(BaseError):
+    FAILED_TEST_LIMIT_EXCESS = 59
+
+    @staticmethod
+    def range():
+        return (50, 59)
+
+class OSTFError(BaseError):
+    FAILED_TEST_LIMIT_EXCESS = 69
+
+    @staticmethod
+    def range():
+        return (60, 69)
+
+class TempestError(BaseError):
+    TESTS_FAILED = 81
+    VERIFICATION_FAILED = 83
+    FAILED_TEST_LIMIT_EXCESS = 89
+
+    @staticmethod
+    def range():
+        return (80, 89)
+
+class ReservedError(BaseError):
+
+    @staticmethod
+    def range():
+        return (90, 99)
+
+class ComplexError(BaseError):
+    SOME_SUITES_FAILED = 100
+
+    @staticmethod
+    def range():
+        return (100, 100)
