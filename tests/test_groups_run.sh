9cad259dcd15f3ab62f638a993286acebf94cc64
IlyaMenkov <imenkov@mirantis.com>
2016-03-28 00:21:54 +0300

    Added ability to run small and full tests
    parameters:
    1. test name: small or full
    2. credentials, example: "(instance_ip=172.18.78.96 controller_ip=2.2.2.2)"
    3. test group, example: "(rally=test_name, ostf=test_name)"
    
    Change-Id: I394e42507f6e7a266b25490ecd80e151434e61b9

--- tests/test_groups_run.sh
+++ tests/test_groups_run.sh
@@ -1,24 +1,28 @@
 #/bin/bash/
 
-declare -a arr=$1
-sudo sed -i "/\[basic\]/acontroller_ip=${arr[0]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/ainstance_ip=${arr[1]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/aos_username=${arr[2]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/aos_tenant_name=${arr[3]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/aos_password=${arr[4]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/aauth_endpoint_ip=${arr[5]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/anailgun_host=${arr[6]}" /etc/mcv/mcv.conf
-sudo sed -i "/\[basic\]/acluster_id=${arr[7]}" /etc/mcv/mcv.conf
-sudo sed -i "s/version=6.1/version=${arr[8]}/" /etc/mcv/mcv.conf
-sudo sed -i "s/private_endpoint_ip=192.168.0.2/private_endpoint_ip=${arr[9]}/" /etc/mcv/mcv.conf
-sudo sed -i "s/concurrency=5/concurrency=1/" /etc/mcv/mcv.conf
-sudo mcvconsoler --run custom quick >> /tmp/cli_output.log
-sudo echo 'quick ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom workload >> /tmp/cli_output.log
-sudo echo 'workload ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom functional >> /tmp/cli_output.log
-sudo echo 'functional ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom smoke >> /tmp/cli_output.log
-sudo echo 'smoke ' $? >> /tmp/test_result.log
-sudo mcvconsoler --run custom scale >> /tmp/cli_output.log
-sudo echo 'scale ' $? >> /tmp/test_result.log
+echo $1 $2 $3 >creds.log
+TEST_NAME=$1
+declare -a credentials=$2
+declare -a test_suite=$3
+for i in ${credentials[@]}
+do
+    sudo sed -i "/\[basic\]/a$i" /etc/mcv/mcv.conf
+done
+
+if [ $TEST_NAME == "small" ]
+then
+    sudo sed -i -e '1 s/^/[custom_test_group_small]\n/;' /etc/mcv/mcv.conf
+    for testname in ${test_suite[@]}
+        do
+            sudo sed -i "/\[custom_test_group_small\]/a$testname" /etc/mcv/mcv.conf
+            sudo mcvconsoler --run custom small >> /tmp/small.log
+            sudo echo "$testname $?" >> /tmp/test_result.log
+            sudo sed -i 2d /etc/mcv/mcv.conf
+        done
+else
+    for testname in ${test_suite[@]}
+        do
+            sudo mcvconsoler --run custom $testname >> /tmp/cli_output.log
+            sudo echo "$testname $?" >> /tmp/test_result.log
+        done
+fi
--- tests/tmux_mcv_tests_runner.sh
+++ tests/tmux_mcv_tests_runner.sh
@@ -1,8 +1,8 @@
 #/bin/bash/
 
-echo $1
+echo $1 $2 $3
 SES_NAME='mcv_test'
 tmux kill-session -t $SES_NAME
 tmux new -d -s $SES_NAME 'sudo -i'
 tmux rename-window 'jump'
-tmux new-window -t $SES_NAME -n "default" "bash /opt/mcv-consoler/tests/test_groups_run.sh \"$1\""
+tmux new-window -t $SES_NAME -n "default" "bash /opt/mcv-consoler/tests/test_groups_run.sh $1 \"$2\" \"$3\""
