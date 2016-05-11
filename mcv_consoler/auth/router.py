cee2cdfce3b2dd208680ed024b8f5451bcaf8e17
Eugeniya Kudryashova <ekudryashova@mirantis.com>
2016-05-11 14:09:05 +0300

    Missed comma
    Minor fix for auth router
    
    Change-Id: Iff3dab7a7ba90ba50b30af1aa806e6da2ad88f70

--- mcv_consoler/auth/router.py
+++ mcv_consoler/auth/router.py
@@ -54,7 +54,7 @@ class IRouter(Router):
                    'fuel': {
                        'nailgun_host': GET(self.config, 'nailgun_host'),
                        'nailgun_port': nailgun_port,
-                       'cluster_id': GET(self.config, 'cluster_id')
+                       'cluster_id': GET(self.config, 'cluster_id'),
                        # TODO(albartash): fix in router.py (None to "")
                        'ca_cert': ""},
 
